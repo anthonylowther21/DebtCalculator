2bd61460b2cacd6a6f68efa7fa70b834327b0063
Anthony Lowther <anthonylowther@icloud.com>
2015-10-11 20:15:22 -0500

    Deleted Duplicates

---
+++ DebtCalculator.Library/DebtSnowball/WindfallEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DebtCalculator.Library
+{
+	public class WindfallEntry
+	{
+        static public WindfallEntry CreateWindfallEntry(double amount, 
+                                                        DateTime windfallDate, 
+                                                        bool isRecurring = false, 
+                                                        int recurringFrequency = -1)
+        {
+            return new WindfallEntry()
+            {
+                Amount = amount,
+                WindfallDate = windfallDate,
+                IsReccurring = isRecurring,
+                ReccurringFrequency = recurringFrequency
+            };
+        }
+
+        protected WindfallEntry ()
+		{
+		}
+
+		public double Amount { get; private set; }
+		public bool IsReccurring { get; private set; }
+		public int ReccurringFrequency { get; private set; }
+		public DateTime WindfallDate { get; private set; }
+	}
+}
+
