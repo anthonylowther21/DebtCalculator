cd7f26a6f5665eddf60bfd3f3561539554cdfba0
Anthony Lowther <anthonylowther@icloud.com>
2016-02-26 18:54:27 -0600

    Debt Page View
    Made debt page look prettier

--- DebtCalculator/Converters/DoubleToPercentConverter.cs
+++ DebtCalculator/Converters/DoubleToPercentConverter.cs
@@ -8,7 +8,7 @@ namespace DebtCalculator.Converters
     public object Convert(object value, Type targetType, object parameter,
       System.Globalization.CultureInfo culture)
     {
-      return (double)value * 100;
+      return ((double)value * 100);
     }
 
     public object ConvertBack(object value, Type targetType, object parameter,
