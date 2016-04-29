477cf415a7e1f52be395db9151fe8889b7cc5681
Anthony Lowther <anthonylowther@icloud.com>
2016-04-29 10:25:33 -0500

    #5 Different Icons for Debts
    Added card icon and bank icon types for debts

---
+++ DebtCalculator/FontAwesome/FontAwesome.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace DebtCalculator
+{
+  public static class FontAwesome
+  {
+    public static string FACalculator = "\uf1ec";
+    public static string FAUndo = "\uf0e2";
+    public static string FAExclamation = "\uf12a";
+    public static string FABars = "\uf0c9";
+    public static string FAResetDefaults = "\uf0e2";
+    public static string FAInfo = "\uf129";
+    public static string FASave = "\uf0c7";
+    public static string FALoad = "\uf115";
+    public static string FAEdit = "\uf044";
+    public static string FASetMin = "\uf063";
+    public static string FASetMax = "\uf062";
+    public static string FAHelp = "\uf059";
+    public static string FAHouse = "\uf015";
+    public static string FACard = "\uf09d";
+    public static string FABank = "\uf19c";
+  }
+}
+
+
