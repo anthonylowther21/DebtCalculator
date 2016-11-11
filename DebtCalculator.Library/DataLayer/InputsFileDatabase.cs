5378f1c261fcf4f656c5cd03921e0f2d69fe5a5e
Anthony Lowther <anthonylowther@icloud.com>
2016-11-11 17:57:56 -0600

    Did a new build with iOS 10.1 and updated packages

--- DebtCalculator.Library/DataLayer/InputsFileDatabase.cs
+++ DebtCalculator.Library/DataLayer/InputsFileDatabase.cs
@@ -56,6 +56,7 @@ namespace DebtCalculatorLibrary.DataLayer
       {
         LoadInputsFile(_previousInputsFile, data);
       }
+      _currentInputsFile = string.Empty;
     }
 
     /// <summary>
